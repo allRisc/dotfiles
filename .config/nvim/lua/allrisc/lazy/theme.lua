62bd9ecb969b5d31cb1f81df3dfd1488a3bac6a7
allRisc <allrisc.dev@gmail.com>
2025-09-15 07:30:10 -0700

    Initial neovim configuration added

---
+++ .config/nvim/lua/allrisc/lazy/theme.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        "catppuccin/nvim", 
+        name = "catppuccin", 
+        priority = 1000 ,
+        config = function() 
+            require("catppuccin").setup({
+                flavour = "mocha"
+            })
+
+            vim.cmd.colorscheme "catppuccin"
+        end
+    },
+    { 
+        "nvim-tree/nvim-web-devicons", 
+        lazy = true 
+    },
+}
