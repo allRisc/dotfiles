ee471faafd9b54364dee6d948796131454aad159
allRisc <allrisc.dev@gmail.com>
2025-09-16 11:25:51 -0700

    Added telescope file browser

--- .config/nvim/lua/allrisc/lazy/telescope.lua
+++ .config/nvim/lua/allrisc/lazy/telescope.lua
@@ -1,20 +1,40 @@
 return {
-    "nvim-telescope/telescope.nvim",
+    {
+        "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+        tag = "0.1.5",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim"
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        },
+
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    file_browser = {
+                        theme = "ivy",
+                        hijack_netrw = true,
+                        mappings = {
+                            ["i"] = {},
+                            ["n"] = {},
+                        },
+                    },
+                },
+            })
+
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+            vim.keymap.set('n', '<leader>ps', function()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") })
+            end)
+        end
     },
-
-    config = function()
-        require("telescope").setup({})
-
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-    end
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+        config = function ()
+            require("telescope").load_extension("file_browser")
+        end,
+    }
 }
--- .config/nvim/lua/allrisc/remap.lua
+++ .config/nvim/lua/allrisc/remap.lua
@@ -1,3 +1,15 @@
+-- Change to leader
 vim.g.mapleader = ' '
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- Exit to the netrw
+vim.keymap.set("n", "<leader>pv", ":Telescope file_browser path=%:p:h select_buffer=true<CR><Esc>")
+
+-- Set Ctrl+W to exit the current buffer
+vim.keymap.set({"n", "v"}, "<C-w>", ":bd<CR>")
+
+-- Keep the cursor at the same point when using 'J' to concat lines
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Change how navigation is handled
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
