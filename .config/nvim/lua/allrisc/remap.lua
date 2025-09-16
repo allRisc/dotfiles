dd22204805296b3a73a9357539fb14c848d3b1a9
Ben Davis <b-davis1@ti.com>
2025-09-16 15:03:15 -0700

    :bug: Changed Ctrl+W back to window management

--- .config/nvim/lua/allrisc/remap.lua
+++ .config/nvim/lua/allrisc/remap.lua
@@ -4,9 +4,6 @@ vim.g.mapleader = ' '
 -- Exit to the netrw
 vim.keymap.set("n", "<leader>pv", ":Telescope file_browser path=%:p:h select_buffer=true<CR><Esc>")
 
--- Set Ctrl+W to exit the current buffer
-vim.keymap.set({"n", "v"}, "<C-w>", ":bd<CR>")
-
 -- Keep the cursor at the same point when using 'J' to concat lines
 vim.keymap.set("n", "J", "mzJ`z")
 
